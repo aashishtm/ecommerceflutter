7b1e0841e5a6575fdcdba79a8b068f6dc76ee7e9
Aashish Thapa Magar <aashishtm@localhost.localdomain>
2020-10-27 20:45:27 +0545

    Side Drawer completed

---
+++ lib/widgets/sidedrawer.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+
+class SideDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        children: [
+          //  Header section of the application
+          UserAccountsDrawerHeader(
+            accountName: Text(
+              'Aashish Thapa',
+            ),
+            accountEmail: Text(
+              'info@aashishtm.com.np',
+            ),
+            currentAccountPicture: GestureDetector(
+              child: CircleAvatar(
+                backgroundColor: Colors.grey,
+                child: Icon(Icons.person, color: Colors.white),
+              ),
+            ),
+            decoration: BoxDecoration(
+              color: Colors.red,
+            ),
+          ),
+          //body section
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Home Page'),
+              leading: Icon(
+                Icons.home,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My Account'),
+              leading: Icon(
+                Icons.person,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('My Orders'),
+              leading: Icon(
+                Icons.shopping_basket,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Categories'),
+              leading: Icon(
+                Icons.dashboard,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Favourites'),
+              leading: Icon(
+                Icons.favorite,
+                color: Colors.red,
+              ),
+            ),
+          ),
+          Divider(),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('Settings'),
+              leading: Icon(
+                Icons.settings,
+                color: Colors.blue[600],
+              ),
+            ),
+          ),
+          InkWell(
+            onTap: () {},
+            child: ListTile(
+              title: Text('About'),
+              leading: Icon(
+                Icons.help,
+                color: Colors.green,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
